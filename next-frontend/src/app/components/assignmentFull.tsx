4309542e5df22d54fc1309cbf2d6c07e760fa727
Marat Shagidullin <alivadjib@gmail.com>
2024-09-07 11:41:58 +0300

    added dashboard layout

--- next-frontend/src/app/components/assignmentFull.tsx
+++ next-frontend/src/app/components/assignmentFull.tsx
@@ -2,6 +2,11 @@
 import { Fragment } from 'react'
 const AssignmentFull = () => {
   return (<Fragment>
+    <header className="bg-white shadow">
+      <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold tracking-tight text-gray-900">Task Assignment</h1>
+      </div>
+    </header>
     <div className="max-w-7xl mx-auto p-4 md:p-6 lg:p-8">
       <h1 className="text-3xl font-bold mb-4">Assignment: Fullstack Developer</h1>
       <h2 className="text-2xl font-bold mb-4">Task: Develop a Task Management Application with Dashboard</h2>
--- next-frontend/src/app/dashboard/page.tsx
+++ next-frontend/src/app/dashboard/page.tsx
@@ -1,8 +1,32 @@
-'use client'
-
+// 'use client'
+import { Fragment } from "react"
 const Dashboard = () => {
   return <>
-   Dashboard Nav left
+   <Fragment>
+    <div className="flex h-screen">
+      <div className="p-4 w-1/4 bg-gray-100 border-r border-gray-300 rounded-2xl">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Add Task
+        </button>
+        <ul className="mt-4">
+          <li className="py-2 border-b border-gray-300">Task</li>
+          <li className="py-2 border-b border-gray-300">Task</li>
+          <li className="py-2 border-b border-gray-300">Task</li>
+          <li className="py-2">Task</li>
+        </ul>
+      </div>
+
+      <div className="p-4 w-full bg-white rounded-2xl">
+        <h2 className="text-2xl font-bold mb-4">Task edit and add</h2>
+        <div className="bg-gray-100 p-4 rounded">
+          {/* <!-- Add your task edit and add form here --> */}
+        </div>
+      </div>
+    </div>
+      
+   </Fragment>
+
+
   </>
 }
 
